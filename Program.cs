4eed3e967aab84193a06d921cc00c7bbd2de3a4e
Dwi Putra Setiadi <61934529+odisetiadi@users.noreply.github.com>
2020-03-23 13:16:02 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tugas_membuat_object_taxi_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+                Taxi taxi = new Taxi();
+
+                taxi.DriverName = "Jono";
+                taxi.OnDuty = true;
+                taxi.NumPassenger = 10;
+
+                taxi.taxiinfo();
+                taxi.PickUpPassenger();
+                taxi.DropOffPassenger();
+
+
+                Console.ReadKey();
+            }
+        }
+    }
---
+++ Taxi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas_membuat_object_taxi_3
+{
+    class Taxi
+    {
+        public string DriverName;
+        public Boolean OnDuty;
+        public float NumPassenger;
+
+        public void taxiinfo()
+        {
+            Console.WriteLine("Driver name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", OnDuty);
+            Console.WriteLine("Number Of Passenger: {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
